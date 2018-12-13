beac91edfd54a2c015d038d1a39b4619f5842a78
baby7305 <songhaibei7305@163.com>
2018-12-13 15:55:34 +0800

    路由初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
+import { Route, Link } from "react-router-dom";
 import './App.css';
 
-class App extends React.Component {
-  public render() {
+import About from "./home/About";
+import Index from "./home/Home";
+import Users from "./home/User";
+
+class AppRouter extends React.Component {
+  render() {
     return (
-      <h1>Hello, world!</h1>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
     );
   }
 }
 
-export default App;
+export default AppRouter;
